3aa50c6dcfae32d13a91df24fd16e78392c4a42c
Silvision <richardmnguyen@hotmail.com>
2021-04-16 22:58:38 -0500

    Improved Layout and started Fragments

--- app/src/main/java/com/example/eternalreturntracker/MainActivity.java
+++ app/src/main/java/com/example/eternalreturntracker/MainActivity.java
@@ -1,8 +1,13 @@
 package com.example.eternalreturntracker;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 import com.google.android.material.bottomnavigation.BottomNavigationItemView;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
@@ -17,6 +22,34 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         bottomNavigationView = findViewById((R.id.bottomNavigation));
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @SuppressLint("NonConstantResourceId")
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                Fragment fragment;
+                switch (item.getItemId()){
+                    case R.id.action_search:
+                        Toast.makeText(MainActivity.this, "Search!", Toast.LENGTH_SHORT).show();
+                        break;
+                    case R.id.action_rankings:
+                        Toast.makeText(MainActivity.this, "Rankings!", Toast.LENGTH_SHORT).show();
+                        break;
+                    case R.id.action_characters:
+                        Toast.makeText(MainActivity.this, "Characters!", Toast.LENGTH_SHORT).show();
+                        break;
+                    case R.id.action_favorites:
+                    default:
+                        Toast.makeText(MainActivity.this, "Favorites!", Toast.LENGTH_SHORT).show();
+                        break;
+
+
+                }
+                return true;
+
+            }
+
+        });
 
     }
 }
---
+++ app/src/main/java/com/example/eternalreturntracker/fragments/searchFragment.java
@@ -0,0 +1,43 @@
+package com.example.eternalreturntracker.fragments;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.ImageView;
+
+import com.example.eternalreturntracker.R;
+
+public class searchFragment extends Fragment {
+
+    private EditText etSearch;
+    private ImageView ivSearchIcon;
+
+
+    public searchFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_search, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        etSearch = view.findViewById(R.id.etSearch);
+        ivSearchIcon = view.findViewById(R.id.ivSearchIcon);
+
+
+
+    }
+}
