960394ff26e9fd94a3a620af2e6b99ab1a062aae
Colton Sustaita-Robb <colton.sustaitarobb@mavs.uta.edu>
2021-04-30 22:33:49 -0500

    Working on character data

--- app/src/main/java/com/example/eternalreturntracker/fragments/CharactersFragment.java
+++ app/src/main/java/com/example/eternalreturntracker/fragments/CharactersFragment.java
@@ -2,15 +2,50 @@ package com.example.eternalreturntracker.fragments;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.eternalreturntracker.BuildConfig;
+import com.example.eternalreturntracker.R;
+import com.example.eternalreturntracker.models.EternalReturnInterface;
+import com.example.eternalreturntracker.models.User;
+import com.example.eternalreturntracker.models.MoreUserDetails;
+import com.google.gson.Gson;
+
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import okhttp3.Interceptor;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.ResponseBody;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
 
 import com.example.eternalreturntracker.R;
 
 public class CharactersFragment extends Fragment {
+    public static final String BASE_URL = "https://open-api.bser.io";
+    public static final String REST_CONSUMER_KEY = BuildConfig.CONSUMER_KEY;
+    private EditText etSearch;
+    private ImageView ivSearchIcon;
 
     public CharactersFragment() {
         // Required empty public constructor
@@ -21,5 +56,54 @@ public class CharactersFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_characters, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        etSearch = view.findViewById(R.id.etSearch);
+        ivSearchIcon = view.findViewById(R.id.ivSearchIcon);
+
+
+        OkHttpClient.Builder httpClient = new OkHttpClient.Builder();
+        httpClient.addInterceptor(new Interceptor() {
+            @Override
+            public okhttp3.Response intercept(Chain chain) throws IOException {
+                Request request = chain.request()
+                        .newBuilder()
+                        .addHeader("x-api-key", REST_CONSUMER_KEY)
+                        .build();
+                return chain.proceed(request);
+            }
+        });
+
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl(BASE_URL)
+                .addConverterFactory(GsonConverterFactory.create())
+                .client(httpClient.build())
+                .build();
+
+        EternalReturnInterface eternalReturnInterface = retrofit.create(EternalReturnInterface.class);
+
+        Call<User> call = eternalReturnInterface.getUser("silvision");
+        call.enqueue(new Callback<User>() {
+            @Override
+            public void onResponse(Call<User> call, Response<User> response) {
+
+                if(response != null) {
+                    Log.i("STUFF", new Gson().toJson(response.body(),User.class));
+                    Log.i("STUFF", response.body().getMoreUserDetails().getNickname());
+                }
+
+
+            }
+
+            @Override
+            public void onFailure(Call<User> call, Throwable t) {
+                Log.i("STUFF", "onFailure for TRYING TO ENQUEUE THINGY", t);
+            }
+        });
+
+
     }
 }
